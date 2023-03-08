b61d4784d9b4602e574c65bfa3fa904c8459e3a1
mohammedwaseem0udy <127044860+mohammedwaseem0udy@users.noreply.github.com>
2023-03-08 23:07:38 +0530

    V1

---
+++ index.js
@@ -0,0 +1,19 @@
+let scrollToBestsellers = function() {
+    let ele = document.getElementById('BestSellers');
+    ele.scrollIntoView()
+}
+
+let scrollToHome = function() {
+    let ele = document.getElementById('Home');
+    ele.scrollIntoView();
+}
+
+let scrollToLocations = function() {
+    let ele = document.getElementById('Locations');
+    ele.scrollIntoView();
+}
+
+let scrollToContact = function() {
+    let ele = document.getElementById('Contact');
+    ele.scrollIntoView();
+}
